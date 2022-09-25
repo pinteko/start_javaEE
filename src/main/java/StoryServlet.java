70d77594ca2aee38d497f62cdc917d0c40ece324
Kirill Korsuk <korsukkirill@gmail.com>
2022-09-25 17:22:45 +0300

    homework

--- src/main/java/StoryServlet.java
+++ src/main/java/StoryServlet.java
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "StoryServlet", urlPatterns = "/story")
 public class StoryServlet implements Servlet {
@@ -23,9 +24,20 @@ public class StoryServlet implements Servlet {
 
     @Override
     public void service(ServletRequest reg, ServletResponse res) throws ServletException, IOException {
-        log.info("start request");
-        res.getWriter().println("<h1>fuck putin</h1>");
-        log.info("end request");
+        ArrayList<Story> stories = new ArrayList<>();
+        stories.add(new Story("Invisible Monsters", "Chuck Palahniuk", 4.8));
+        stories.add(new Story("Under the Dome", "Stephen King", 4.6));
+        stories.add(new Story("Three comrades", "Remark", 4.8));
+        stories.add(new Story("The Thanatonauts", "Bernard Werber", 4.9));
+        stories.add(new Story("The Three Musketeers", "Alexandre Dumas", 4.7));
+        stories.add(new Story("Graf Monte-Cristo", "Alexandre Dumas", 5.0));
+        stories.add(new Story("The Brothers Karamazov", "Fyodor Dostoevsky", 4.8));
+        stories.add(new Story("Choke", "Chuck Palahniuk", 4.7));
+        stories.add(new Story("Arch of Triumph", "Remark", 4.8));
+        stories.add(new Story("The catcher in the rye", "Jerome Salinger", 4.6));
+        for (int i = 0; i < stories.size(); i++) {
+            res.getWriter().println(stories.get(i).toString());
+        }
     }
 
     @Override
